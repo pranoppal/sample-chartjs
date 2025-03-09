b896b5dc3ba004aa128ca468412a2d9f8939dfe6
Pranoppal Kalita <pranoppal1006@gmail.com>
2025-03-09 13:27:51 +0530

    Update temp.java

--- temp.java
+++ temp.java
@@ -1,19 +1,26 @@
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: my-app
-spec:
-  replicas: 2
-  selector:
-    matchLabels:
-      app: my-app
-  template:
-    metadata:
-      labels:
-        app: my-app
-    spec:
-      containers:
-      - name: my-app
-        image: <ACR_NAME>.azurecr.io/<YOUR_IMAGE>:latest  # Change this to your image
-        ports:
-        - containerPort: 80
+replicaCount: 2
+
+image:
+  repository: myregistry.azurecr.io/my-springboot-app
+  tag: latest
+  pullPolicy: IfNotPresent
+
+service:
+  type: LoadBalancer
+  port: 8080
+
+env:
+  - name: SPRING_RABBITMQ_HOST
+    value: my-rabbitmq
+  - name: SPRING_RABBITMQ_PORT
+    value: "5672"
+  - name: SPRING_RABBITMQ_USERNAME
+    valueFrom:
+      secretKeyRef:
+        name: my-rabbitmq
+        key: rabbitmq-username
+  - name: SPRING_RABBITMQ_PASSWORD
+    valueFrom:
+      secretKeyRef:
+        name: my-rabbitmq
+        key: rabbitmq-password
