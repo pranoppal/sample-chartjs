c4b4eab629163559bfca730c97d1712eaf6b66ea
Pranoppal Kalita <pranoppal1006@gmail.com>
2025-03-24 22:21:30 +0530

    Update temp.java

--- temp.java
+++ temp.java
@@ -3,7 +3,7 @@ import org.apache.hc.client5.http.config.Registry;
 import org.apache.hc.client5.http.config.RegistryBuilder;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingAsyncClientConnectionManager;
 import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
 import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
 import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
@@ -20,33 +20,33 @@ import java.security.KeyStore;
 public class SecureRestTemplateJKS {
 
     public static RestTemplate createRestTemplate(String keystorePath, String keystorePassword) throws Exception {
-        // Load JKS Keystore (for client authentication)
-        KeyStore keyStore = KeyStore.getInstance("JKS"); // Specify JKS instead of PKCS12
+        // Load JKS Keystore
+        KeyStore keyStore = KeyStore.getInstance("JKS");
         File keyStoreFile = Paths.get(keystorePath).toFile();
         try (var keyStoreInputStream = java.nio.file.Files.newInputStream(keyStoreFile.toPath())) {
             keyStore.load(keyStoreInputStream, keystorePassword.toCharArray());
         }
 
-        // Create SSLContext with the JKS keystore
+        // Create SSLContext
         SSLContext sslContext = SSLContexts.custom()
                 .loadKeyMaterial(keyStore, keystorePassword.toCharArray()) // Client cert
-                .loadTrustMaterial((chain, authType) -> true) // Trust all certs (not for production)
+                .loadTrustMaterial((chain, authType) -> true) // Trust all certs (for testing)
                 .build();
 
         // Create SSLConnectionSocketFactory
         SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(sslContext);
 
-        // Configure connection manager
+        // Configure connection manager using PoolingAsyncClientConnectionManager
         Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
                 .register("https", sslSocketFactory)
                 .register("http", new PlainConnectionSocketFactory())
                 .build();
 
-        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+        PoolingAsyncClientConnectionManager connectionManager = new PoolingAsyncClientConnectionManager(socketFactoryRegistry);
         connectionManager.setDefaultMaxPerRoute(10);
         connectionManager.setMaxTotal(50);
 
-        // Create Apache HttpClient 5 with SSL
+        // Create Apache HttpClient 5
         CloseableHttpClient httpClient = HttpClients.custom()
                 .setConnectionManager(connectionManager)
                 .setDefaultConnectionConfig(ConnectionConfig.custom()
