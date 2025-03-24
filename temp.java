315582e4fafd1cf5b2359bb7870c7d2bc792d5ad
Pranoppal Kalita <pranoppal1006@gmail.com>
2025-03-24 20:42:03 +0530

    Update temp.java

--- temp.java
+++ temp.java
@@ -1,1 +1,5 @@
-kubectl exec -it $(kubectl get pods -n your-namespace -l app.kubernetes.io/name=rabbitmq -o jsonpath="{.items[0].metadata.name}") -n your-namespace -- rabbitmq-plugins list
+az network private-dns record-set a add-record \
+  --resource-group <RESOURCE_GROUP> \
+  --zone-name <PRIVATE_DNS_ZONE> \
+  --record-set-name "" \
+  --ipv4-address <IP_ADDRESS>
