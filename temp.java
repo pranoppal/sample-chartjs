0f84f760380e39c9a693a3b0ada6566ca9985969
Pranoppal Kalita <pranoppal1006@gmail.com>
2025-03-24 22:27:23 +0530

    Update temp.java

--- temp.java
+++ temp.java
@@ -1,14 +1,14 @@
 import org.apache.hc.client5.http.config.ConnectionConfig;
-import org.apache.hc.client5.http.config.Registry;
+import org.apache.hc.client5.http.config.Lookup;
 import org.apache.hc.client5.http.config.RegistryBuilder;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingAsyncClientConnectionManager;
-import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
 import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.DefaultTlsStrategy;
 import org.apache.hc.core5.ssl.SSLContexts;
 import org.apache.hc.core5.util.Timeout;
+import org.apache.hc.core5.util.TimeValue;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
@@ -33,18 +33,18 @@ public class SecureRestTemplateJKS {
                 .loadTrustMaterial((chain, authType) -> true) // Trust all certs (for testing)
                 .build();
 
-        // Create SSLConnectionSocketFactory
-        SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(sslContext);
-
-        // Configure connection manager using PoolingAsyncClientConnectionManager
-        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
-                .register("https", sslSocketFactory)
-                .register("http", new PlainConnectionSocketFactory())
+        // Configure TLS strategy
+        Lookup<org.apache.hc.client5.http.ssl.TlsStrategy> tlsStrategyLookup = RegistryBuilder.<org.apache.hc.client5.http.ssl.TlsStrategy>create()
+                .register("https", new DefaultTlsStrategy(sslContext))
                 .build();
 
-        PoolingAsyncClientConnectionManager connectionManager = new PoolingAsyncClientConnectionManager(socketFactoryRegistry);
-        connectionManager.setDefaultMaxPerRoute(10);
-        connectionManager.setMaxTotal(50);
+        // Create Connection Manager with TlsStrategy
+        PoolingAsyncClientConnectionManager connectionManager = PoolingAsyncClientConnectionManager.builder()
+                .setTlsStrategyRegistry(tlsStrategyLookup)
+                .setMaxTotal(50)
+                .setDefaultMaxPerRoute(10)
+                .setValidateAfterInactivity(TimeValue.ofSeconds(5))
+                .build();
 
         // Create Apache HttpClient 5
         CloseableHttpClient httpClient = HttpClients.custom()
